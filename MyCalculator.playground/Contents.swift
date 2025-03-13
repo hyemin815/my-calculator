3e0c48b5335bf4ddc5e08005257b6b65bb046c34
hyemin <hmpark15@gmail.com>
2025-03-13 17:25:20 +0900

    Lv3 first modified

--- MyCalculator.playground/Contents.swift
+++ MyCalculator.playground/Contents.swift
@@ -6,20 +6,29 @@ class Calculator {
         self.num1 = num1
         self.num2 = num2
     }
-    
-    func sum() -> Double {
+}
+
+class AddOperation: Calculator {
+    func add() -> Double {
         return num1 + num2
     }
-    
-    func difference() -> Double {
+}
+
+class SubtractOperation: Calculator {
+    func subtract() -> Double {
         return num1 - num2
     }
-    
-    func product() -> Double {
+}
+
+class MultiplyOperation: Calculator {
+    func multiply() -> Double {
         return num1 * num2
     }
-    
-    func quotient() -> Double {
+}
+
+class DivideOperation: Calculator {
+    // num2가 0일 경우 나누기와 나머지 연산자에서 오류 발생하여 조건문 처리
+    func divide() -> Double {
         if num2 == 0 {
             print("정의되지 않음")
             return 0
@@ -27,7 +36,9 @@ class Calculator {
             return num1 / num2
         }
     }
-    
+}
+
+class RemainderOperation: Calculator {
     func remainder() -> Double {
         if num2 == 0 {
             print("정의되지 않음")
@@ -38,13 +49,7 @@ class Calculator {
     }
 }
 
-let calculator = Calculator(num1: 1.24, num2: 1)
 
-print(calculator.sum())
-print(calculator.difference())
-print(calculator.product())
-print(calculator.quotient())
-print(calculator.remainder())
-
-// num1이 0, 마이너스, 소수점일 때 문제 없이 계산됨
-// num2가 마이너스, 소수점일 때는 잘 계산되었으나 0일 경우 나누기와 나머지 연산자에서 오류 발생
+let calculator = AddOperation(num1: 6, num2: 3)
+let calculatorSubtract = SubtractOperation(num1: 10, num2: 3)
+print(calculatorSubtract.subtract())
